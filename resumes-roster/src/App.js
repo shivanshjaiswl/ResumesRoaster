5c58f91fd34852224aac670606a0b1c41c1943fc
shivanshjaiswl <115590236+shivanshjaiswl@users.noreply.github.com>
2024-11-09 17:20:56 -0500

    react

--- resumes-roster/src/App.js
+++ resumes-roster/src/App.js
@@ -1,11 +1,46 @@
 // src/components/home.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const SimpleCenteredText = () => {
+  const [text, setText] = useState('');
+  const [showButton, setShowButton] = useState(false);
+  const [fade, setFade] = useState(false);
+  const fullText = "Hi, I'm an A.I. trained to evaluate resumes. To get started, I'll need to log into your Gmail to see all your application rejections.<br /><br />I'm just gonna look at your rejection emails. I won't post or change anything.";
+
+  useEffect(() => {
+    let index = 0;
+
+    const typeWriter = () => {
+      if (index < fullText.length) {
+        const currentText = fullText.substring(0, index + 1).replace(/<br \/>/g, '\n');
+        setText(currentText);
+        index++;
+        setTimeout(typeWriter, 50);
+      } else {
+        setShowButton(true);
+      }
+    };
+
+    typeWriter();
+  }, []);
+
   return (
     <div style={styles.centeredPage}>
-      <h1 style={styles.centeredText}><p>Welcome to the Resumes Roste Hi, I'm an A.I. trained to evaluate resumes.</p>  <p>To get started, I'll need to log into your Gmail to see all your application rejections.
-        I'm just gonna look at your rejection emails. I won't post or change anything.</p></h1>
+      <div style={styles.textContainer}>
+        <p style={styles.centeredText}>
+          {text.split('\n').map((item, idx) => (
+            <React.Fragment key={idx}>
+              {item}
+              <br />
+            </React.Fragment>
+          ))}
+        </p>
+        {showButton && (
+          <button style={styles.loginButton}>
+            Login
+          </button>
+        )}
+      </div>
     </div>
   );
 };
@@ -13,13 +48,29 @@ const SimpleCenteredText = () => {
 const styles = {
   centeredPage: {
     display: 'flex',
+    justifyContent: 'center',
     alignItems: 'center',
-    height: '100vh',
-    width: '80vh'
+    height: '40vh',
+    textAlign: 'center',
+    padding: '20px',
+  },
+  textContainer: {
+    maxWidth: '600px',
   },
   centeredText: {
-    fontSize: '12px',
-    color: '#333',
+    fontSize: '16px',
+    color: '#555',
+    textAlign: 'left', // Align text to the left
+  },
+  loginButton: {
+    marginTop: '20px',
+    padding: '10px 20px',
+    fontSize: '16px',
+    backgroundColor: '#4CAF50',
+    color: 'white',
+    border: 'none',
+    cursor: 'pointer',
+    borderRadius: '5px',
   },
 };
 
